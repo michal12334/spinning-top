2c3e183f4fd668fa8c7f4016b18631f156097f9f
Michal Boguszewski <michal1842001@gmail.com>
2024-11-16 12:49:10 +0100

    Set trajectory aplha to 1

--- src/trajectory_drawer.rs
+++ src/trajectory_drawer.rs
@@ -30,7 +30,7 @@ impl TrajectoryDrawer {
             out vec4 color;
             
             void main() {
-                color = vec4(1, 1, 1, 0.5);
+                color = vec4(1, 1, 1, 1);
             }
         "#;
 
